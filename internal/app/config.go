2ff5dc2a0f08c4e63cb5550c8d811a5d293aed56
Alfa <huchashev_ad@bk.ru>
2023-08-07 13:41:46 +0300

    Some fixes with variable names and logic

--- internal/app/config.go
+++ internal/app/config.go
@@ -11,17 +11,17 @@ type Configure struct {
 }
 
 func (c *Configure) SetConfig() {
-	server_address, isexist := os.LookupEnv("SERVER_ADDRESS")
-	if isexist == false {
+	serverAddress, isexist := os.LookupEnv("SERVER_ADDRESS")
+	if isexist {
+		c.Host = serverAddress
+	} else {
 		flag.StringVar(&c.Host, "a", "localhost:8080", "for listenandserve")
-	} else {
-		c.Host = server_address
 	}
-	base_url, isexist := os.LookupEnv("BASE_URL")
-	if isexist == false {
-		flag.StringVar(&c.Address, "b", "http://localhost:8080", "for response")
+	baseUrl, isexist := os.LookupEnv("BASE_URL")
+	if isexist {
+		c.Address = baseUrl
 	} else {
-		c.Address = base_url
+		flag.StringVar(&c.Address, "b", "http://localhost:8080", "for response")
 	}
 	flag.Parse()
 	c.Address += "/"
