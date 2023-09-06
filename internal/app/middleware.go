5665944e4af6b3630c972d542431047a1a0abba6
Alfa <huchashev_ad@bk.ru>
2023-09-07 00:37:23 +0300

    Добавил компрессию данных

--- internal/app/middleware.go
+++ internal/app/middleware.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -21,4 +22,29 @@ func (b *BaseController) WithLogging(h http.HandlerFunc) http.HandlerFunc {
 			"storage", b.storage.Urls)
 	}
 	return logfn
+}
+
+func gzipMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ow := w
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			cw := newCompressWriter(w)
+			ow = cw
+			defer cw.Close()
+		}
+		contentEncoding := r.Header.Get("Content-Encoding")
+		sendsGzip := strings.Contains(contentEncoding, "gzip")
+		if sendsGzip {
+			cr, err := newCompressReader(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			r.Body = cr
+			defer cr.Close()
+		}
+		h.ServeHTTP(ow, r)
+	}
 }
--- internal/app/router.go
+++ internal/app/router.go
@@ -5,9 +5,9 @@ import "github.com/go-chi/chi"
 func (b *BaseController) Route() *chi.Mux {
 	r := chi.NewRouter()
 	r.Route("/", func(r chi.Router) {
-		r.Post("/", b.WithLogging(b.solvePost))
-		r.Get("/{shorturl}", b.WithLogging(b.solveGet))
-		r.Post("/api/shorten", b.WithLogging(b.solveJSON))
+		r.Post("/", gzipMiddleware(b.WithLogging(b.solvePost)))
+		r.Get("/{shorturl}", gzipMiddleware(b.WithLogging(b.solveGet)))
+		r.Post("/api/shorten", gzipMiddleware(b.WithLogging(b.solveJSON)))
 	})
 	return r
 }
