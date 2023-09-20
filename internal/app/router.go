e9357162d4cca26de131a8d6381045e30f6d22fc
Alfa <huchashev_ad@bk.ru>
2023-09-20 21:43:53 +0300

    little fixes

--- internal/app/router.go
+++ internal/app/router.go
@@ -7,7 +7,7 @@ func (b *BaseController) Route() *chi.Mux {
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", gzipMiddleware(b.WithLogging(b.solvePost)))
 		r.Get("/{shorturl}", gzipMiddleware(b.WithLogging(b.solveGet)))
-		r.Get("/ping", b.solvePing)
+		r.Get("/ping", b.WithLogging(b.solvePing))
 		r.Post("/api/shorten", gzipMiddleware(b.WithLogging(b.solveJSON)))
 		r.Post("/api/shorten/batch", gzipMiddleware(b.WithLogging(b.solveBatch)))
 	})
